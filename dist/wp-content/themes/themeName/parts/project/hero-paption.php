bd7208f8e1c2d50c26caf033ec2e5bfd38c23cde
Yoshiaki Numajiri <afmaar128@gmail.com>
2025-07-20 17:42:04 +0900

    t_2025-01-11wpからいろいろ移植

--- dist/wp-content/themes/themeName/parts/project/hero-paption.php
+++
@@ -1,5 +0,0 @@
-  <div class="p-hero-caption l-hero-caption">
-    <div class="p-hero-caption__text">hero caption sample text</div>
-    <div class="p-hero-caption__text">hero caption sample text</div>
-    <div class="p-hero-caption__text">hero caption sample text</div>
-  </div>
--- src/js/index.js
+++
@@ -1,5 +0,0 @@
-import './_drawer.js';
-import './_slider.js';
-import './_modal.js';
-
-console.log('index.jsからこんにちは！');
