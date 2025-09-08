cfcbd3d0b0ca5809ffa342229ca732f6ae11059d
Yoshiaki Numajiri <afmaar128@gmail.com>
2025-09-08 20:42:56 +0900

    fix: テーマファイル

--- src/js/_drawer.js
+++ src/js/_drawer.js
@@ -1,16 +1,20 @@
-console.log("drawer");
 // ドロワーメニュー関連の処理
 const menuButton = document.getElementById("js-menu");
+const menuButtonRight = window.getComputedStyle(menuButton).right;
 const drawer = document.getElementById("js-drawer");
 const drawerMenu = document.getElementById("js-drawer-menu");
 const drawerAnchorLinks = drawerMenu.querySelectorAll('a[href*="#"]');
 
 // ドロワーメニューを展開する処理
 function openDrawer() {
+  const scrollbarWidth =
+    window.innerWidth - document.documentElement.clientWidth;
   menuButton.setAttribute("aria-expanded", "true");
   drawer.setAttribute("aria-hidden", "false");
   document.documentElement.style.overflow = "hidden";
   document.body.style.overflow = "hidden";
+  document.body.style.paddingRight = scrollbarWidth + "px";
+  menuButton.style.right = parseFloat(menuButtonRight) + scrollbarWidth + "px";
 }
 
 // ドロワーメニューを閉じる処理
@@ -19,6 +23,8 @@ function closeDrawer() {
   drawer.setAttribute("aria-hidden", "true");
   document.documentElement.style.overflow = "";
   document.body.style.overflow = "";
+  document.body.style.paddingRight = "";
+  menuButton.style.right = parseFloat(menuButtonRight) + "px";
 }
 
 // ハンバーガーメニューをクリックした時の処理
@@ -40,10 +46,11 @@ drawerAnchorLinks.forEach(function (link) {
 // ドロワーメニュー以外の要素をクリックしたとき、ドロワーメニューを閉じる
 drawer.addEventListener("click", function (event) {
   if (
-    drawerMenu && drawerMenu.contains(event.target) ||
-    menuButton && menuButton.contains(event.target)
-  ) return;
-  
+    (drawerMenu && drawerMenu.contains(event.target)) ||
+    (menuButton && menuButton.contains(event.target))
+  )
+    return;
+
   closeDrawer();
 });
 
@@ -52,4 +59,4 @@ window.addEventListener("resize", function () {
   if (window.innerWidth >= 768) {
     closeDrawer();
   }
-}); 
+});
