eaaa6c15b8abb3103f4d8842820e1e7cf641b803
Yoshiaki Numajiri <afmaar128@gmail.com>
2025-10-02 18:56:53 +0900

    サムネイル表示関数 altを投稿タイトルではなく、設定されているaltに

--- dist/wp-content/themes/themeName/functions-lib/func-thumbnail.php
+++ dist/wp-content/themes/themeName/functions-lib/func-thumbnail.php
@@ -29,17 +29,16 @@ function get_thumbnail_data($size = 'full') {
         $width         = $src ? (int) $src[1] : null;
         $height        = $src ? (int) $src[2] : null;
         $class         = '';
-        // alt は未設定のケースがあるので、投稿タイトルをフォールバック
-        $alt_text      = get_the_title(); // 投稿タイトルをaltに設定
-        // $alt_text      = get_post_meta($thumbnail_id, '_wp_attachment_image_alt', true); // 画像に設定されているaltを使用する場合
-        // if ($alt_text === '' || $alt_text === null) {
-        //     $alt_text = get_the_title($thumbnail_id) ?: '';
-        // }
+        // alt は未設定のケースがあるので、タイトルをフォールバック
+        $alt_text      = get_post_meta($thumbnail_id, '_wp_attachment_image_alt', true);
+        if ($alt_text === '' || $alt_text === null) {
+            $alt_text = get_the_title($thumbnail_id) ?: '';
+        }
     } else {
         // フォールバック（例: SVG の場合は width/height を省略してもOK）
         $url      = get_template_directory_uri() . '/assets/images/common/logo.svg';
-        $width    = 106;   // ここを null にして属性を省略したいなら null に
-        $height   = 88;
+        $width    = 371;   // ここを null にして属性を省略したいなら null に
+        $height   = 239;
         $class    = 'u-no-image';
         $alt_text = 'no image';
     }
