b43af41d60edc29c0d36db51f8d35951b1c1d0cc
Yoshiaki Numajiri <afmaar128@gmail.com>
2025-11-19 08:52:45 +0900

    functions-lib/func-ai1wm-exclude.php コメントアウト

--- dist/wp-content/themes/themeName/functions-lib/func-ai1wm-exclude.php
+++ dist/wp-content/themes/themeName/functions-lib/func-ai1wm-exclude.php
@@ -1,17 +1,18 @@
 <?php
 /*
- *All-in-One WP Migrationのエクスポートデータから除外するディレクトリを設定
+ * All-in-One WP Migrationのエクスポートデータから除外するディレクトリを設定
+ * テーマファイル内に除外したいファイルはないので、コメントアウト
 */
-add_filter(
-	'ai1wm_exclude_themes_from_export',
-	function ($exclude_filters) {
-		$theme_dir = basename(get_theme_root() . '/' . get_stylesheet()); // 実際のテーマディレクトリ名を取得
-		$exclude_filters[] = $theme_dir . '/.git';
-		$exclude_filters[] = $theme_dir . '/.gitignore';
-		$exclude_filters[] = $theme_dir . '/gulp';
-		$exclude_filters[] = $theme_dir . '/src';
-		$exclude_filters[] = $theme_dir . '/README.md';
+// add_filter(
+// 	'ai1wm_exclude_themes_from_export',
+// 	function ($exclude_filters) {
+// 		$theme_dir = basename(get_theme_root() . '/' . get_stylesheet()); // 実際のテーマディレクトリ名を取得
+// 		$exclude_filters[] = $theme_dir . '/.git';
+// 		$exclude_filters[] = $theme_dir . '/.gitignore';
+// 		$exclude_filters[] = $theme_dir . '/gulp';
+// 		$exclude_filters[] = $theme_dir . '/src';
+// 		$exclude_filters[] = $theme_dir . '/README.md';
 
-		return $exclude_filters;
-	}
-);
+// 		return $exclude_filters;
+// 	}
+// );
